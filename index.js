a2c7cb33af6075485d65de2289e2de7b9a8c6fb3
Aneerudh17 <paneerudh@gmail.com>
2025-06-12 13:52:33 +0530

    adding cursor

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const maxLength = 26;
 
 $(document).on('keydown', function(event) {
     const key = event.key;
-    const currentText = $typedText.text();
 
+    let currentText = $typedText.text();
+    
     if ((currentText.length >= maxLength) && !(key === 'Backspace' || key === 'Enter')) {
         event.preventDefault();
         return;
@@ -13,13 +14,13 @@ $(document).on('keydown', function(event) {
 
     if (key.length === 1 || key === 'Backspace' || key === 'Enter') {
         event.preventDefault();
-
         if (key === 'Backspace') {
             $typedText.text(currentText.slice(0, -1));
         } else if (key === 'Enter') {
             $typedText.text('');
         } else {
-            $typedText.append(key);
+            $typedText.text(currentText + key);
         }
+        $output.append($('.cursor'));
     }
 });
